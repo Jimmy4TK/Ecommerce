bead2e5a9b027b105af018f08b85b850f346b346
Jimmy4TK <juanfalco@hotmail.com.ar>
2022-08-31 22:19:48 -0300

    Spring inicializer

---
+++ Jump-Start/src/test/java/com/ecommerce/JumpStart/JumpStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.JumpStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
